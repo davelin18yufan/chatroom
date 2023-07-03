49b22a8df9023ecf9b104659c14e96bbd046b30a
davelin18yufan <davelin30630@gmail.com>
2023-07-03 18:03:37 +0800

    feat:add socket.io and connection established

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -1,4 +1,12 @@
 import "./index.css";
 import { name } from "@/utils";
+import { io } from "socket.io-client"
 
 console.log("client side chatroom page", name);
+
+const clientIo = io()
+
+// 建立前端頁面連線
+clientIo.on("join", (msg) => {
+  console.log("here", msg)
+})
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import devServer from "./server/dev";
 import prodServer from "./server/prod";
 import express from "express";
+import { Server } from "socket.io";
+import http from "http";
 
 import { name } from "@/utils";
 
 const port = 3000;
 const app = express();
+// 建立伺服器端
+const server = http.createServer(app);
+const io = new Server(server);
+
+// 監聽連接
+io.on("connection", (socket) => {
+  socket.emit("join", "welcome") // 在連線join頻道後發送訊息給客戶端
+})
 
 // 執行npm run dev本地開發 or 執行npm run start部署後啟動線上伺服器
 if (process.env.NODE_ENV === "development") {
@@ -16,6 +26,6 @@ if (process.env.NODE_ENV === "development") {
 
 console.log("server side", name);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`The application is running on port ${port}.`);
 });
