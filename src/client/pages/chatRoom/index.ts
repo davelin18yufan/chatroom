48652d8e08f250bf71cf3dc3d0ff56c00aed34e0
davelin18yufan <davelin30630@gmail.com>
2023-07-03 21:54:21 +0800

    feat:add right side chat function

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -6,8 +6,6 @@ const url = new URL(location.href)
 const userName = url.searchParams.get("user_name")
 const roomName = url.searchParams.get("room_name")
 
-console.log("chat", userName, roomName)
-
 // 沒有接收到值就返回首頁
 if (!userName || !roomName) {
   location.href = `/main/main.html`
@@ -15,7 +13,55 @@ if (!userName || !roomName) {
 
 const clientIo = io()
 
+//取得聊天室輸入匡
+const textInput = document.getElementById("textInput") as HTMLInputElement
+const submitBtn = document.getElementById("submitBtn") as HTMLButtonElement
+const chatBoard = document.getElementById("chatBoard") as HTMLDivElement
+const headerRoomName = document.getElementById("headerRoomName") as HTMLParagraphElement
+const backBtn = document.getElementById("backBtn") as HTMLButtonElement
+
+headerRoomName.innerText = roomName || " - "
+
+//處理送出訊息
+function msgHandler(msg: string){
+  const divBox = document.createElement("div")
+  divBox.classList.add("flex", "justify-end", "mb-4", "items-end")
+  divBox.innerHTML = `
+    <p class="text-xs text-gray-700 mr-4">00:00</p>
+    <div>
+      <p class="text-xs text-white mb-1 text-right">Dave</p>
+      <p
+        class="mx-w-[50%] break-all bg-white px-4 py-2 rounded-bl-full rounded-br-full rounded-tl-full"
+      >
+        ${msg}
+      </p>
+    </div>
+  `
+
+  chatBoard.appendChild(divBox)
+  //清空輸入匡
+  textInput.value = ""
+  //把畫面往下推
+  chatBoard.scrollTop = chatBoard.scrollHeight
+}
+
+submitBtn.addEventListener("click", () => {
+  const textValue = textInput.value
+  
+  // 拿到職後發送到伺服器端
+  clientIo.emit("chat", textValue) //chat event
+})
+
+backBtn.addEventListener("click", () => {
+  location.href = `/main/main.html`
+})
+
 // 建立前端頁面連線
 clientIo.on("join", (msg) => {
-  console.log("here", msg)
+})
+
+clientIo.on("chat", (msg) => {
+  console.log("client", msg)
+  // 究收後端回傳的訊息丟到聊天室
+  msgHandler(msg)
 })
--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,11 @@ const io = new Server(server);
 // 監聽連接
 io.on("connection", (socket) => {
   socket.emit("join", "welcome") // 在連線join頻道後發送訊息給客戶端
+
+  // 連接後監聽chat頻道的訊息
+  socket.on("chat", (msg) => {
+    io.emit("chat", msg) // 因為前後端接受有時間差，使用一樣的事件觸發沒關係
+  })
 })
 
 // 執行npm run dev本地開發 or 執行npm run start部署後啟動線上伺服器
